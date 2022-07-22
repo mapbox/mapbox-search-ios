56f786a324a90cede389873ac39deae8d5ff9beb
Maxim Makhun <maxim.makhun@mapbox.com>
2022-07-22 16:48:05 -0700

    Update to Mapbox Common `v22.1.0-rc.1`.

--- MapboxSearch.podspec
+++ MapboxSearch.podspec
@@ -24,6 +24,6 @@ Some iOS platform specifics applies.
 
   s.vendored_frameworks = "**/#{s.name}.xcframework"
 
-  s.dependency "MapboxCommon", "22.1.0-beta.1"
+  s.dependency "MapboxCommon", "22.1.0-rc.1"
 
 end
--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "MapboxCommon", url: "https://github.com/mapbox/mapbox-common-ios.git", .exact("22.1.0-beta.1")),
+        .package(name: "MapboxCommon", url: "https://github.com/mapbox/mapbox-common-ios.git", .exact("22.1.0-rc.1")),
         .package(url: "https://github.com/mattgallagher/CwlPreconditionTesting.git", from: "2.0.0")
     ],
     targets: [
