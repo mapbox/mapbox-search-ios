00b747411fb7da02e822ff4ae9e00d0ed8eb38a7
Jack Alto <jack.alto@mapbox.com>
2024-02-14 18:27:25 -0500

    Pare down GeocodingMockResponse, remove mocks that are not geocoding ApiType

--- Tests/MapboxSearchUITests/MockWebServer/MockResponse.swift
+++ Tests/MapboxSearchUITests/MockWebServer/MockResponse.swift
@@ -20,116 +20,32 @@ protocol MockResponse {
 }
 
 enum GeocodingMockResponse: MockResponse {
-    case suggestEmpty
-    case suggestMinsk
-    case suggestSanFrancisco
-    case suggestCategories
-    case suggestWithCoordinates
-    case suggestWithMixedCoordinates
-    case suggestCategoryWithCoordinates
-    case suggestAddressSanFrancisco
-
-    case retrieveSanFrancisco
-    case retrieveMinsk
-    case retrieveCategory
-    case retrievePoi
-    case multiRetrieve
-    case retrieveAddressSanFrancisco
-
-    case recursion
     case forwardGeocoding
     case reverseGeocoding
     case reverseGeocodingSBS
-    case categoryCafe
 
     var filepath: String {
         let bundle = Bundle(for: MockWebServer<Self>.self)
         switch self {
-        case .suggestEmpty:
-            return bundle.path(forResource: "suggestions-empty", ofType: "json")!
-        case .suggestMinsk:
-            return bundle.path(forResource: "suggestions-minsk", ofType: "json")!
-        case .suggestSanFrancisco:
-            return bundle.path(forResource: "suggestions-san-francisco", ofType: "json")!
-        case .suggestCategories:
-            return bundle.path(forResource: "suggestions-categories", ofType: "json")!
-        case .suggestWithCoordinates:
-            return bundle.path(forResource: "suggestions-with-coordinates", ofType: "json")!
-        case .suggestWithMixedCoordinates:
-            return bundle.path(forResource: "suggestions-with-mixed-coordinates", ofType: "json")!
-        case .suggestCategoryWithCoordinates:
-            return bundle.path(forResource: "suggestions-category-with-coordinates", ofType: "json")!
-        case .retrieveSanFrancisco:
-            return bundle.path(forResource: "retrieve-san-francisco", ofType: "json")!
-        case .retrieveMinsk:
-            return bundle.path(forResource: "retrieve-minsk", ofType: "json")!
-        case .retrieveCategory:
-            return bundle.path(forResource: "retrieve-category", ofType: "json")!
-        case .retrievePoi:
-            return bundle.path(forResource: "retrieve-poi", ofType: "json")!
-        case .recursion:
-            return bundle.path(forResource: "recursion", ofType: "json")!
         case .reverseGeocodingSBS:
             return bundle.path(forResource: "reverse-geocoding-sbs", ofType: "json")!
         case .reverseGeocoding:
             return bundle.path(forResource: "geocoding-reverse-geocoding", ofType: "json")!
         case .forwardGeocoding:
             return bundle.path(forResource: "mapbox.places.san.francisco", ofType: "json")!
-        case .multiRetrieve:
-            return bundle.path(forResource: "retrieve-multi", ofType: "json")!
-        case .categoryCafe:
-            return bundle.path(forResource: "category-cafe", ofType: "json")!
-        case .suggestAddressSanFrancisco:
-            return bundle.path(forResource: "address-suggestions-san-francisco", ofType: "json")!
-        case .retrieveAddressSanFrancisco:
-            return bundle.path(forResource: "address-retrieve-san-francisco", ofType: "json")!
         }
     }
 
     var path: String {
         var path = "/search/v1"
-
         switch self {
-        case .suggestAddressSanFrancisco:
-            path = "/autofill/v1/suggest/:query"
-
-        case .retrieveAddressSanFrancisco:
-            path = "/autofill/v1/retrieve/:action.id"
-
         case .forwardGeocoding:
             path = "/geocoding/v5/mapbox.places/:query"
-
-        case .suggestMinsk:
-            path += "/suggest/Minsk"
-
-        case .suggestSanFrancisco:
-            path += "/suggest/San Francisco"
-
-        case .suggestEmpty,
-             .suggestCategories,
-             .suggestWithCoordinates,
-             .suggestWithMixedCoordinates,
-             .suggestCategoryWithCoordinates,
-             .recursion:
-            path += "/suggest/:query"
-
-        case .retrieveSanFrancisco,
-             .retrieveCategory,
-             .retrieveMinsk,
-             .retrievePoi:
-            path += "/retrieve"
-
         case .reverseGeocoding:
-            path = "geocoding/v5/mapbox.places/:location"
+            path = "/geocoding/v5/mapbox.places/:location"
 
         case .reverseGeocodingSBS:
             path += "/:coordinates"
-
-        case .multiRetrieve:
-            path += "/retrieve/multi"
-
-        case .categoryCafe:
-            path += "/category/:category"
         }
 
         return path
@@ -137,28 +53,10 @@ enum GeocodingMockResponse: MockResponse {
 
     var httpMethod: HttpServer.HTTPMethod {
         switch self {
-        case .suggestAddressSanFrancisco,
-             .retrieveAddressSanFrancisco,
-             .forwardGeocoding,
-             .suggestMinsk,
-             .suggestSanFrancisco,
-             .suggestEmpty,
-             .suggestCategories,
-             .suggestWithCoordinates,
-             .suggestWithMixedCoordinates,
-             .suggestCategoryWithCoordinates,
-             .recursion,
+        case .forwardGeocoding,
              .reverseGeocoding,
-             .reverseGeocodingSBS,
-             .categoryCafe:
+             .reverseGeocodingSBS:
             return .get
-
-        case .multiRetrieve,
-             .retrieveSanFrancisco,
-             .retrieveCategory,
-             .retrieveMinsk,
-             .retrievePoi:
-            return .post
         }
     }
 
