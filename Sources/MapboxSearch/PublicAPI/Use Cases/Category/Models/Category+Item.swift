2cd40398e90b7e30926c76602fd9f3879a29c1ff
Jack Alto <jack.alto@mapbox.com>
2024-02-29 15:13:00 -0500

    Update for PR feedback: rely on localized category names
    - Move Categories.strings to MapboxSearch
    - Move Strings.Categories enum helper to MapboxSearch

--- Sources/MapboxSearch/PublicAPI/Use Cases/Category/Models/Category+Item.swift
+++ Sources/MapboxSearch/PublicAPI/Use Cases/Category/Models/Category+Item.swift
@@ -30,19 +30,29 @@ extension Category.Item {
 
     /// Fuel station category.
     public static let gas =
-        Category.Item(canonicalId: "gas_station", name: "gas", legacyName: "fuel", iconName: Maki.fuel)
+        Category.Item(canonicalId: "gas_station", name: Strings.Categories.gas, legacyName: "fuel", iconName: Maki.fuel)
 
     /// Parking lot category.
     public static let parking =
-        Category.Item(canonicalId: "parking_lot", name: "parking", legacyName: "parking", iconName: Maki.parking)
+        Category.Item(
+            canonicalId: "parking_lot",
+            name: Strings.Categories.parking,
+            legacyName: "parking",
+            iconName: Maki.parking
+        )
 
     /// Food category.
     public static let food =
-        Category.Item(canonicalId: "restaurant", name: "food", legacyName: "restaurant", iconName: Maki.restaurant)
+        Category.Item(
+            canonicalId: "restaurant",
+            name: Strings.Categories.food,
+            legacyName: "restaurant",
+            iconName: Maki.restaurant
+        )
 
     /// Cafe category.
     public static let cafe =
-        Category.Item(canonicalId: "cafe", name: "cafe", legacyName: "cafe", iconName: Maki.cafe)
+        Category.Item(canonicalId: "cafe", name: Strings.Categories.cafe, legacyName: "cafe", iconName: Maki.cafe)
 
     // MARK: - Other categories
 
@@ -50,117 +60,162 @@ extension Category.Item {
     public static let restaurant =
         Category.Item(
             canonicalId: "restaurant",
-            name: "restaurant",
+            name: Strings.Categories.restaurant,
             legacyName: "restaurant",
             iconName: Maki.restaurant
         )
 
     /// Bar category.
     public static let bar =
-        Category.Item(canonicalId: "bar", name: "bar", legacyName: "bar", iconName: Maki.bar)
+        Category.Item(canonicalId: "bar", name: Strings.Categories.bar, legacyName: "bar", iconName: Maki.bar)
 
     /// Cafe category.
     public static let coffeeShop =
-        Category.Item(canonicalId: "cafe", name: "coffeeShop", legacyName: "cafe", iconName: Maki.cafe)
+        Category.Item(canonicalId: "cafe", name: Strings.Categories.coffeeShop, legacyName: "cafe", iconName: Maki.cafe)
 
     /// Hotel category.
     public static let hotel =
-        Category.Item(canonicalId: "hotel", name: "hotel", legacyName: "hotel", iconName: Maki.lodging)
+        Category.Item(canonicalId: "hotel", name: Strings.Categories.hotel, legacyName: "hotel", iconName: Maki.lodging)
 
     /// Gas Station category.
     public static let gasStation =
-        Category.Item(canonicalId: "gas_station", name: "gasStation", legacyName: "fuel", iconName: Maki.fuel)
+        Category.Item(
+            canonicalId: "gas_station",
+            name: Strings.Categories.gasStation,
+            legacyName: "fuel",
+            iconName: Maki.fuel
+        )
 
     /// EV Charging station category.
     public static let chargingStation =
         Category.Item(
             canonicalId: "ev_charging_station",
-            name: "chargingStation",
+            name: Strings.Categories.chargingStation,
             legacyName: "charging station",
             iconName: Maki.chargingStation
         )
 
     /// Bus station category.
     public static let busStation =
-        Category.Item(canonicalId: "bus_station", name: "busStation", legacyName: "bus station", iconName: Maki.bus)
+        Category.Item(
+            canonicalId: "bus_station",
+            name: Strings.Categories.busStation,
+            legacyName: "bus station",
+            iconName: Maki.bus
+        )
 
     /// Railway station category.
     public static let railwayStation =
         Category.Item(
             canonicalId: "railway_station",
-            name: "railwayStation",
+            name: Strings.Categories.railwayStation,
             legacyName: "train station",
             iconName: Maki.rail
         )
 
     /// Shopping mall category.
     public static let shoppingMall =
-        Category.Item(canonicalId: "shopping_mall", name: "shoppingMall", legacyName: "mall", iconName: Maki.shop)
+        Category.Item(
+            canonicalId: "shopping_mall",
+            name: Strings.Categories.shoppingMall,
+            legacyName: "mall",
+            iconName: Maki.shop
+        )
 
     /// Grocery category.
     public static let grocery =
-        Category.Item(canonicalId: "grocery", name: "grocery", legacyName: "grocery", iconName: Maki.grocery)
+        Category.Item(
+            canonicalId: "grocery",
+            name: Strings.Categories.grocery,
+            legacyName: "grocery",
+            iconName: Maki.grocery
+        )
 
     /// Clothing store category.
     public static let clothingStore =
         Category.Item(
             canonicalId: "clothing_store",
-            name: "clothingStore",
+            name: Strings.Categories.clothingStore,
             legacyName: "clothes",
             iconName: Maki.clothingStore
         )
 
     /// Pharmacy category.
     public static let pharmacy =
-        Category.Item(canonicalId: "pharmacy", name: "pharmacy", legacyName: "pharmacy", iconName: Maki.pharmacy)
+        Category.Item(
+            canonicalId: "pharmacy",
+            name: Strings.Categories.pharmacy,
+            legacyName: "pharmacy",
+            iconName: Maki.pharmacy
+        )
 
     /// Museum category.
     public static let museum =
-        Category.Item(canonicalId: "museum", name: "museum", legacyName: "museum", iconName: Maki.museum)
+        Category.Item(
+            canonicalId: "museum",
+            name: Strings.Categories.museum,
+            legacyName: "museum",
+            iconName: Maki.museum
+        )
 
     /// Park category.
     public static let park =
-        Category.Item(canonicalId: "park", name: "park", legacyName: "park", iconName: Maki.park)
+        Category.Item(canonicalId: "park", name: Strings.Categories.park, legacyName: "park", iconName: Maki.park)
 
     /// Cinema category.
     public static let cinema =
-        Category.Item(canonicalId: "cinema", name: "cinema", legacyName: "cinema", iconName: Maki.cinema)
+        Category.Item(
+            canonicalId: "cinema",
+            name: Strings.Categories.cinema,
+            legacyName: "cinema",
+            iconName: Maki.cinema
+        )
 
     /// Fitness center category.
     public static let fitnessCentre =
         Category.Item(
             canonicalId: "fitness_center",
-            name: "fitnessCentre",
+            name: Strings.Categories.fitnessCentre,
             legacyName: "fitness center",
             iconName: Maki.fitnessCentre
         )
 
     /// Nightclub category.
     public static let nightclub =
-        Category.Item(canonicalId: "nightclub", name: "nightclub", legacyName: "nightclub", iconName: Maki.nightclub)
+        Category.Item(
+            canonicalId: "nightclub",
+            name: Strings.Categories.nightclub,
+            legacyName: "nightclub",
+            iconName: Maki.nightclub
+        )
 
     /// Auto repair category.
     public static let autoRepair =
         Category.Item(
             canonicalId: "auto_repair",
-            name: "autoRepair",
+            name: Strings.Categories.autoRepair,
             legacyName: "auto repair",
             iconName: Maki.carRepair
         )
 
     /// ATM category.
     public static let atm =
-        Category.Item(canonicalId: "atm", name: "atm", legacyName: "atm", iconName: Maki.bank)
+        Category.Item(canonicalId: "atm", name: Strings.Categories.atm, legacyName: "atm", iconName: Maki.bank)
 
     /// Hospital category.
     public static let hospital =
-        Category.Item(canonicalId: "hospital", name: "hospital", legacyName: "hospital", iconName: Maki.hospital)
+        Category.Item(
+            canonicalId: "hospital",
+            name: Strings.Categories.hospital,
+            legacyName: "hospital",
+            iconName: Maki.hospital
+        )
 
     /// Emergency Room category.
     public static let emergencyRoom =
         Category.Item(
             canonicalId: "emergency_room",
-            name: "emergencyRoom",
+            name: Strings.Categories.emergencyRoom,
             legacyName: "emergency room",
             iconName: Maki.emergencyPhone
         )
