98661f8905488aec32746a2d004b62008eda4778
Jack Alto <jack.alto@mapbox.com>
2024-03-12 11:19:27 -0400

    Add ~/.netrc set up for Xcode cloud

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -3,5 +3,11 @@
 # Propagate environment secrets
 echo $MAPBOX_API_TOKEN > ~/.mapbox
 
+# Set up netrc for packages
+echo "machine api.mapbox.com" >> ~/.netrc
+echo "login mapbox" >> ~/.netrc
+echo "password $MAPBOX_API_TOKEN" >> ~/.netrc
+chmod 0600 ~/.netrc
+
 # Fetch Carthage dependencies
 make dependencies
