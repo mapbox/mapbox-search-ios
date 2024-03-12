e32301e2f387665ad8411f35cbd404c7edcd8cce
Jack Alto <jack.alto@mapbox.com>
2024-03-12 12:14:13 -0400

    Exit script if token is not found

--- Sources/ci_scripts/ci_post_clone.sh
+++ Sources/ci_scripts/ci_post_clone.sh
@@ -2,6 +2,11 @@
 
 # Support Xcode Cloud for Sources/Demo.xcodeproj
 
+if [[ -z $MAPBOX_API_TOKEN ]];then
+	echo "Please provide an API token environment variable in Xcode Cloud"
+    exit 1
+fi
+
 # Propagate environment secrets
 echo $MAPBOX_API_TOKEN > ~/.mapbox
 
@@ -12,4 +17,5 @@ echo "password $MAPBOX_API_TOKEN" >> ~/.netrc
 chmod 0600 ~/.netrc
 
 # Fetch Carthage dependencies
+cd ../../
 make dependencies
