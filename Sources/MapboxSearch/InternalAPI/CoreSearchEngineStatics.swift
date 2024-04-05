972e72d3d2097e564103c448494a05e2f3a83c44
Jack Alto <jack.alto@mapbox.com>
2024-04-05 13:06:01 -0400

    Change delimiter to underscore - SSDK-622

--- Sources/MapboxSearch/InternalAPI/CoreSearchEngineStatics.swift
+++ Sources/MapboxSearch/InternalAPI/CoreSearchEngineStatics.swift
@@ -1,11 +1,15 @@
 import Foundation
 
 enum CoreSearchEngineStatics {
+    enum Constants {
+        static let delimiter = "_"
+    }
+
     static func createTilesetDescriptor(dataset: String, version: String, language: String? = nil) -> MapboxCommon
     .TilesetDescriptor {
         let identifier: String
         if let language {
-            identifier = dataset + "|" + language
+            identifier = dataset + Constants.delimiter + language
         } else {
             identifier = dataset
         }
@@ -16,10 +20,25 @@ enum CoreSearchEngineStatics {
     .TilesetDescriptor {
         let identifier: String
         if let language {
-            identifier = dataset + "|" + language
+            identifier = dataset + Constants.delimiter + language
         } else {
             identifier = dataset
         }
         return CoreSearchEngine.createPlacesTilesetDescriptor(forDataset: identifier, version: version)
+    }
+}
+
+enum DatasetLanguages {
+    static func filter(language: String) -> String? {
+        var validLanguage: Bool
+        if #available(iOS 16, *) {
+            validLanguage = Locale.LanguageCode.isoLanguageCodes
+                .map(\.identifier)
+                .contains(language)
+        } else {
+            validLanguage = Locale.isoLanguageCodes
+                .contains(language)
+        }
+        return validLanguage ? language : nil
     }
 }
