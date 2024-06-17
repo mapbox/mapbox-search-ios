e97dd38415a8a14f4662860fc223e60839908971
Jack Alto <384288+aokj4ck@users.noreply.github.com>
2024-06-17 14:51:16 -0400

    Expose public api to get Image resource for Maki icon (#250)
    ### Description
    Change MapboxSearchUI `Maki.image: UIImage` field to public
    
    ### Checklist
    - [x] Update `CHANGELOG`

--- Sources/MapboxSearchUI/Maki+Extensions.swift
+++ Sources/MapboxSearchUI/Maki+Extensions.swift
@@ -5,7 +5,10 @@ extension Maki {
     /// Name in Assets Catalog.
     public var xcassetName: String { name }
 
-    var icon: UIImage {
+    /// UIImage representing this Maki icon from scalable SVG source.
+    /// Names and images may change over time.
+    /// Please see https://github.com/mapbox/maki for more information.
+    public var icon: UIImage {
         UIImage(named: xcassetName, in: .mapboxSearchUI, compatibleWith: nil)!
     }
 }
