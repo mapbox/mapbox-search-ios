3d9e5c32a0f04bedd7896f1bfdb6a4837837763b
Jack Alto <jack.alto@mapbox.com>
2024-07-25 12:39:09 -0400

    Update MapboxCoreSearch to v2.3.0-rc.3

--- Tests/MapboxSearchTests/Common/Stubs&Models/CoreSearchResultMetadataStub.swift
+++ Tests/MapboxSearchTests/Common/Stubs&Models/CoreSearchResultMetadataStub.swift
@@ -1,6 +1,41 @@
 import Foundation
 @testable import MapboxSearch
 
+/// Test-only convenience
+extension CoreResultMetadataProtocol {
+    var priceLevel: String? { nil }
+    var popularity: NSNumber? { nil }
+
+    // MARK: Characteristics and Options
+
+    var openHours: CoreOpenHours? { nil }
+    var parkingData: CoreParkingData? { nil }
+    var wheelchairAccessible: NSNumber? { nil }
+    var delivery: NSNumber? { nil }
+    var driveThrough: NSNumber? { nil }
+    var reservable: NSNumber? { nil }
+    var parkingAvailable: NSNumber? { nil }
+    var valetParking: NSNumber? { nil }
+    var streetParking: NSNumber? { nil }
+    var servesBreakfast: NSNumber? { nil }
+    var servesBrunch: NSNumber? { nil }
+    var servesDinner: NSNumber? { nil }
+    var servesLunch: NSNumber? { nil }
+    var servesWine: NSNumber? { nil }
+    var servesBeer: NSNumber? { nil }
+    var servesVegan: NSNumber? { nil }
+    var servesVegetarian: NSNumber? { nil }
+    var takeout: NSNumber? { nil }
+
+    // MARK: Social Media and Contact
+
+    var facebookId: String? { nil }
+    var fax: String? { nil }
+    var email: String? { nil }
+    var instagram: String? { nil }
+    var twitter: String? { nil }
+}
+
 class CoreSearchResultMetadataStub: CoreResultMetadataProtocol {
     init(
         data: [String: String],
@@ -11,7 +46,8 @@ class CoreSearchResultMetadataStub: CoreResultMetadataProtocol {
         reviewCount: NSNumber? = nil,
         phone: String? = nil,
         website: String? = nil,
-        openHours: CoreOpenHours? = nil
+        openHours: CoreOpenHours? = nil,
+        children: [CoreResultChildMetadata]? = nil
     ) {
         self.data = data
         self.primaryImage = primaryImage
@@ -32,8 +68,8 @@ class CoreSearchResultMetadataStub: CoreResultMetadataProtocol {
     var reviewCount: NSNumber?
     var phone: String?
     var website: String?
-
     var openHours: CoreOpenHours?
+    var children: [CoreResultChildMetadata]?
 
     static let sample1 = CoreSearchResultMetadataStub(
         data: [
