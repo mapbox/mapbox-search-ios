a4f557289fe1a3e863d9b95599334d883b0d736c
Jack Alto <jack.alto@mapbox.com>
2024-09-20 13:58:59 -0400

    Add offline user record layer tests
    - Depends on CoreSearch v2.5.0-rc.4

--- Tests/MapboxSearchTests/Legacy/UserRecordsLayerTests.swift
+++ Tests/MapboxSearchTests/Legacy/UserRecordsLayerTests.swift
@@ -5,6 +5,9 @@ final class SearchBox_UserRecordsLayerTests: XCTestCase {
     let delegate = SearchEngineDelegateStub()
     var searchEngine: SearchEngine!
 
+    let dcLocation = CLLocationCoordinate2D(latitude: 38.89992081005698, longitude: -77.03399849939174)
+    let regionId = "dc"
+
     override func setUpWithError() throws {
         try super.setUpWithError()
 
@@ -17,9 +20,12 @@ final class SearchBox_UserRecordsLayerTests: XCTestCase {
     }
 
     /// Set the ignoreUR flag to FALSE to opt-in to to user records layer
-    var searchOptionsWithUserRecords: SearchOptions = .init(ignoreIndexableRecords: false)
+    var searchOptionsWithUserRecords: SearchOptions = .init(
+        ignoreIndexableRecords: false,
+        indexableRecordsDistanceThreshold: nil
+    )
 
-    func testWritingUserRecordsLayer() throws {
+    func testWritingUserRecordsLayer_Online() throws {
         let mockRecordProvider = DataLayerProviderStub(records: [])
 
         let recordsInteractor = try searchEngine.register(dataProvider: mockRecordProvider, priority: 1)
@@ -33,7 +39,7 @@ final class SearchBox_UserRecordsLayerTests: XCTestCase {
 
         XCTAssertNotNil(recordsInteractor)
 
-        recordsInteractor.update(record: IndexableRecordStub.sample1)
+        recordsInteractor.update(record: IndexableRecordStub.sample1_online)
 
         let secondUpdateExpectation = delegate.updateExpectation
         searchEngine.search(query: "obelisk")
@@ -42,10 +48,105 @@ final class SearchBox_UserRecordsLayerTests: XCTestCase {
         let nameResultsAfterAddingUserRecord = searchEngine.suggestions.map(\.name)
         XCTAssertTrue(nameResultsAfterAddingUserRecord.contains(where: { $0 == "Washington Monument" }))
     }
+
+    func testWritingUserRecordsLayer_Offline() throws {
+        let mockRecordProvider = DataLayerProviderStub(records: [])
+
+        let recordsInteractor = try searchEngine.register(dataProvider: mockRecordProvider, priority: 1)
+
+        // Set up Offline
+        let enableOfflineExpectation = expectation(description: "TileStore setup completion")
+        searchEngine.setOfflineMode(.enabled) {
+            enableOfflineExpectation.fulfill()
+        }
+        wait(for: [enableOfflineExpectation], timeout: 10)
+
+        // TestTileStore builds tileStores with unique path allowing runs tests in parallel
+        let tileStore = TestTileStore.build()
+        let setTileStoreExpectation = expectation(description: "TileStore setup completion")
+        searchEngine.offlineManager.setTileStore(tileStore) {
+            setTileStoreExpectation.fulfill()
+        }
+        wait(for: [setTileStoreExpectation], timeout: 10)
+
+        // Set up index observer before the fetch starts to validate changes after it completes
+        let indexChangedExpectation = expectation(description: "Received offline index changed event")
+        let offlineIndexObserver = OfflineIndexObserver(onIndexChangedBlock: { changeEvent in
+            _Logger.searchSDK.info("Index changed: \(changeEvent)")
+            indexChangedExpectation.fulfill()
+        }, onErrorBlock: { error in
+            _Logger.searchSDK.error("Encountered error in OfflineIndexObserver \(error)")
+            XCTFail(error.debugDescription)
+        })
+        searchEngine.offlineManager.engine.addOfflineIndexObserver(for: offlineIndexObserver)
+
+        // Perform the offline fetch
+        let loadDataExpectation = expectation(description: "Load Data")
+        _ = loadData { result in
+            switch result {
+            case .success(let region):
+                XCTAssert(region.id == self.regionId)
+                XCTAssert(region.completedResourceCount > 0)
+                XCTAssertEqual(region.requiredResourceCount, region.completedResourceCount)
+            case .failure(let error):
+                XCTFail("Unable to load Region, \(error.localizedDescription)")
+            }
+            loadDataExpectation.fulfill()
+        }
+        wait(
+            for: [loadDataExpectation, indexChangedExpectation],
+            timeout: 200,
+            enforceOrder: true
+        )
+
+        // Perform the first search without any user records
+        let updateExpectation = delegate.offlineUpdateExpectation
+        searchEngine.search(query: "user record location")
+        wait(for: [updateExpectation], timeout: 300)
+        XCTAssertFalse(searchEngine.suggestions.isEmpty)
+        let nameResultsBeforeUserRecord = searchEngine.suggestions.map { ($0.name, $0.distance) }
+        XCTAssertFalse(nameResultsBeforeUserRecord.contains(where: { $0.0 == "User Record Location" }))
+
+        XCTAssertNotNil(recordsInteractor)
+
+        recordsInteractor.update(record: IndexableRecordStub.sample2_offline)
+
+        let secondUpdateExpectation = delegate.updateExpectation
+        searchEngine.search(query: "user record location")
+        wait(for: [secondUpdateExpectation], timeout: 10)
+        XCTAssertFalse(searchEngine.suggestions.isEmpty)
+        let nameResultsAfterAddingUserRecord = searchEngine.suggestions.map { ($0.name, $0.distance) }
+        XCTAssertTrue(nameResultsAfterAddingUserRecord.contains(where: { $0.0 == "User Record Location" }))
+    }
+
+    // MARK: Helpers
+
+    private func loadData(
+        tilesetDescriptor: TilesetDescriptor? = nil,
+        completion: @escaping (Result<MapboxCommon.TileRegion, MapboxSearch.TileRegionError>) -> Void
+    )
+    -> SearchCancelable {
+        /// A nil tilesetDescriptor parameter will fallback to the default dataset defined at
+        /// ``SearchOfflineManager.defaultDatasetName``
+        let descriptor = tilesetDescriptor ?? SearchOfflineManager.createDefaultTilesetDescriptor()
+
+        let dcLocationValue = NSValue(mkCoordinate: dcLocation)
+        let options = MapboxCommon.TileRegionLoadOptions.build(
+            geometry: Geometry(point: dcLocationValue),
+            descriptors: [descriptor],
+            acceptExpired: true
+        )!
+
+        let cancelable = searchEngine.offlineManager.tileStore.loadTileRegion(id: regionId, options: options) { _ in
+        } completion: { result in
+            completion(result)
+        }
+        return cancelable
+    }
 }
 
 extension IndexableRecordStub {
-    static var sample1 = IndexableRecordStub(
+    static var sample1_online = IndexableRecordStub(
         id: "1234",
         name: "Washington Monument",
         coordinate: CLLocationCoordinate2DCodable(latitude: 38.889462, longitude: -77.03524),
@@ -63,6 +164,27 @@ extension IndexableRecordStub {
             searchAddressCountry: nil // skipped for brevity
         ),
         additionalTokens: ["Monument", "Obelisk"],
+        type: .POI
+    )
+
+    static var sample2_offline = IndexableRecordStub(
+        id: "1234",
+        name: "User Record POI",
+        coordinate: CLLocationCoordinate2DCodable(latitude: 38.889462, longitude: -77.03524),
+        address: Address(
+            houseNumber: "2",
+            street: "15th St NW",
+            neighborhood: "National Mall",
+            locality: nil,
+            postcode: "20024",
+            place: "Washington",
+            district: nil,
+            region: "District of Columbia",
+            searchAddressRegion: nil, // skipped for brevity
+            country: "US",
+            searchAddressCountry: nil // skipped for brevity
+        ),
+        additionalTokens: ["User Record Location"],
         type: .POI
     )
 }
