5c6bf3bdb7f597625459b41f13348ca5f3f8ac08
Jack Alto <jack.alto@mapbox.com>
2024-10-16 14:03:26 -0400

    Update offline tests, add changeEvent.type=updated check
    - Reorder index change events to occur before load data expectation

--- Tests/MapboxSearchIntegrationTests/OfflineIntegrationTests.swift
+++ Tests/MapboxSearchIntegrationTests/OfflineIntegrationTests.swift
@@ -69,10 +69,19 @@ class OfflineIntegrationTests: MockServerIntegrationTestCase<SBSMockResponse> {
         clearData()
 
         // Set up index observer before the fetch starts to validate changes after it completes
-        let indexChangedExpectation = expectation(description: "Received offline index changed event")
+        let indexChanged_AddedExpectation = expectation(description: "Received offline index changed event, type=added")
+        let indexChanged_UpdatedExpectation =
+            expectation(description: "Received offline index changed event, type=updated")
         let offlineIndexObserver = OfflineIndexObserver(onIndexChangedBlock: { changeEvent in
             _Logger.searchSDK.info("Index changed: \(changeEvent)")
-            indexChangedExpectation.fulfill()
+            switch changeEvent.type {
+            case .added:
+                indexChanged_AddedExpectation.fulfill()
+            case .updated:
+                indexChanged_UpdatedExpectation.fulfill()
+            default:
+                return
+            }
         }, onErrorBlock: { error in
             _Logger.searchSDK.error("Encountered error in OfflineIndexObserver \(error)")
             XCTFail(error.debugDescription)
@@ -93,7 +102,7 @@ class OfflineIntegrationTests: MockServerIntegrationTestCase<SBSMockResponse> {
             loadDataExpectation.fulfill()
         }
         wait(
-            for: [loadDataExpectation, indexChangedExpectation],
+            for: [indexChanged_AddedExpectation, indexChanged_UpdatedExpectation, loadDataExpectation],
             timeout: 200,
             enforceOrder: true
         )
@@ -113,10 +122,19 @@ class OfflineIntegrationTests: MockServerIntegrationTestCase<SBSMockResponse> {
         clearData()
 
         // Set up index observer before the fetch starts to validate changes after it completes
-        let indexChangedExpectation = expectation(description: "Received offline index changed event")
+        let indexChanged_AddedExpectation = expectation(description: "Received offline index changed event, type=added")
+        let indexChanged_UpdatedExpectation =
+            expectation(description: "Received offline index changed event, type=updated")
         let offlineIndexObserver = OfflineIndexObserver(onIndexChangedBlock: { changeEvent in
             _Logger.searchSDK.info("Index changed: \(changeEvent)")
-            indexChangedExpectation.fulfill()
+            switch changeEvent.type {
+            case .added:
+                indexChanged_AddedExpectation.fulfill()
+            case .updated:
+                indexChanged_UpdatedExpectation.fulfill()
+            default:
+                return
+            }
         }, onErrorBlock: { error in
             _Logger.searchSDK.error("Encountered error in OfflineIndexObserver \(error)")
             XCTFail(error.debugDescription)
@@ -141,7 +159,7 @@ class OfflineIntegrationTests: MockServerIntegrationTestCase<SBSMockResponse> {
             loadDataExpectation.fulfill()
         }
         wait(
-            for: [loadDataExpectation, indexChangedExpectation],
+            for: [indexChanged_AddedExpectation, indexChanged_UpdatedExpectation, loadDataExpectation],
             timeout: 200,
             enforceOrder: true
         )
@@ -192,10 +210,19 @@ class OfflineIntegrationTests: MockServerIntegrationTestCase<SBSMockResponse> {
         clearData()
 
         // Set up index observer before the fetch starts to validate changes after it completes
-        let indexChangedExpectation = expectation(description: "Received offline index changed event")
+        let indexChanged_AddedExpectation = expectation(description: "Received offline index changed event, type=added")
+        let indexChanged_UpdatedExpectation =
+            expectation(description: "Received offline index changed event, type=updated")
         let offlineIndexObserver = OfflineIndexObserver(onIndexChangedBlock: { changeEvent in
             _Logger.searchSDK.info("Index changed: \(changeEvent)")
-            indexChangedExpectation.fulfill()
+            switch changeEvent.type {
+            case .added:
+                indexChanged_AddedExpectation.fulfill()
+            case .updated:
+                indexChanged_UpdatedExpectation.fulfill()
+            default:
+                return
+            }
         }, onErrorBlock: { error in
             _Logger.searchSDK.error("Encountered error in OfflineIndexObserver \(error)")
             XCTFail(error.debugDescription)
@@ -216,7 +243,7 @@ class OfflineIntegrationTests: MockServerIntegrationTestCase<SBSMockResponse> {
             loadDataExpectation.fulfill()
         }
         wait(
-            for: [loadDataExpectation, indexChangedExpectation],
+            for: [indexChanged_AddedExpectation, indexChanged_UpdatedExpectation, loadDataExpectation],
             timeout: 200,
             enforceOrder: true
         )
