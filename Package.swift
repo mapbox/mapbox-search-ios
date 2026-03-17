mapbox/mapbox-maps-ios

[skip ci][no ci] Bump snapshot versions `SNAPSHOT-03-10--04-52.git-1a4b194`
GitOrigin-RevId: 0c7d9a48d0ec34a15f4ed38e603d94ecb495f475

Package.swift
@@ -4,8 +4,8 @@
 import PackageDescription
 import Foundation
 
-let commonVersion: Version = "24.21.0-SNAPSHOT-03-06--15-09.git-c08be41"
-let coreMapsVersion: Version = "11.21.0-SNAPSHOT-03-06--15-09.git-c08be41"
+let commonVersion: Version = "24.21.0-SNAPSHOT-03-10--04-52.git-1a4b194"
+let coreMapsVersion: Version = "11.21.0-SNAPSHOT-03-10--04-52.git-1a4b194"
 let turfVersion: Version = "4.0.0"
 
 let mapboxMapsPath: String? = nil

mapbox/mapbox-search-ios

Package.swift
@@ -4,8 +4,8 @@
 import PackageDescription
 import Foundation
 
-let coreSearchVersion: Version = "2.21.0-SNAPSHOT-03-06--15-09.git-c08be41"
-let mapboxCommonSDKVersion: Version = "24.21.0-SNAPSHOT-03-06--15-09.git-c08be41"
+let coreSearchVersion: Version = "2.21.0-SNAPSHOT-03-10--04-52.git-1a4b194"
+let mapboxCommonSDKVersion: Version = "24.21.0-SNAPSHOT-03-10--04-52.git-1a4b194"
 
 let package = Package(
     name: "MapboxSearch",