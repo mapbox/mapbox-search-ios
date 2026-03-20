dc966760b18840323d10d7bd16f068ad76d88fe7
Artem Panfilov <artem.panfilov@mapbox.com>
2026-03-20 20:24:31 +0200

    Bump version
    GitOrigin-RevId: 84345ac51cae807a4be93f4544e29fdcac607796

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@
 import PackageDescription
 import Foundation
 
-let coreSearchVersion: Version = "2.21.0-alpha.2"
-let mapboxCommonSDKVersion: Version = "24.21.0-alpha.2"
+let coreSearchVersion: Version = "2.21.0-alpha.3"
+let mapboxCommonSDKVersion: Version = "24.21.0-alpha.3"
 
 let package = Package(
     name: "MapboxSearch",
