mapbox/mapbox-maps-ios

Update local versions to 25.0-SNAPSHOT-05-19--06-51.git-92385b2.
GitOrigin-RevId: 4e4715583e422d712479fd1ab9503b2bb96cd3e2

Package.swift
@@ -4,8 +4,8 @@
 import PackageDescription
 import Foundation
 
-let commonVersion: Version = "24.25.0-SNAPSHOT-05-13--02-05.git-6f98316"
-let coreMapsVersion: Version = "11.25.0-SNAPSHOT-05-13--02-05.git-6f98316"
+let commonVersion: Version = "24.25.0-SNAPSHOT-05-19--06-51.git-92385b2"
+let coreMapsVersion: Version = "11.25.0-SNAPSHOT-05-19--06-51.git-92385b2"
 let turfVersion: Version = "4.0.0"
 
 let mapboxMapsPath: String? = nil

mapbox/mapbox-search-ios

Package.swift
@@ -4,8 +4,8 @@
 import PackageDescription
 import Foundation
 
-let coreSearchVersion: Version = "2.25.0-SNAPSHOT-05-13--02-05.git-6f98316"
-let mapboxCommonSDKVersion: Version = "24.25.0-SNAPSHOT-05-13--02-05.git-6f98316"
+let coreSearchVersion: Version = "2.25.0-SNAPSHOT-05-19--06-51.git-92385b2"
+let mapboxCommonSDKVersion: Version = "24.25.0-SNAPSHOT-05-19--06-51.git-92385b2"
 
 let package = Package(
     name: "MapboxSearch",