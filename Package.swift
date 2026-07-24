6585ec62344386b13286d130606e4505ac17f93b
mapbox-ci <no-reply@mapbox.com>
2026-07-24 08:01:46 +0000

    [skip ci][no ci] Bump snapshot versions `SNAPSHOT-07-24--02-03.git-19b7441`
    GitOrigin-RevId: 1203226631f691c189857c1ecc1fae09715ed945

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@
 import PackageDescription
 import Foundation
 
-let coreSearchVersion: Version = "2.28.0-SNAPSHOT-07-22--19-46.git-c9efd9a"
-let mapboxCommonSDKVersion: Version = "24.28.0-SNAPSHOT-07-22--19-46.git-c9efd9a"
+let coreSearchVersion: Version = "2.28.0-SNAPSHOT-07-24--02-03.git-19b7441"
+let mapboxCommonSDKVersion: Version = "24.28.0-SNAPSHOT-07-24--02-03.git-19b7441"
 
 let package = Package(
     name: "MapboxSearch",
--- Sources/MapboxSearch/PublicAPI/MapboxSearchVersion.swift
+++ Sources/MapboxSearch/PublicAPI/MapboxSearchVersion.swift
@@ -1,2 +1,2 @@
 /// Mapbox Search SDK version variable
-public let mapboxSearchSDKVersion = "2.28.0-SNAPSHOT-07-22--19-46.git-c9efd9a"
+public let mapboxSearchSDKVersion = "2.28.0-SNAPSHOT-07-24--02-03.git-19b7441"
