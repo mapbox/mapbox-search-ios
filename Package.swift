ad32a9f83915deda2e603aeb94dbf7d07abbb0ef
Viktor Kononov <viktor.kononov@mapbox.com>
2026-09-02 11:00:29 +0200

    2.31.0-SNAPSHOT-09-02--01-40.git-f8d37f9 version bump

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@
 import PackageDescription
 import Foundation
 
-let coreSearchVersion: Version = "2.31.0-SNAPSHOT-08-28--04-02.git-352cbc6"
-let mapboxCommonSDKVersion: Version = "24.31.0-SNAPSHOT-08-28--04-02.git-352cbc6"
+let coreSearchVersion: Version = "2.31.0-SNAPSHOT-09-02--01-40.git-f8d37f9"
+let mapboxCommonSDKVersion: Version = "24.31.0-SNAPSHOT-09-02--01-40.git-f8d37f9"
 
 let package = Package(
     name: "MapboxSearch",
