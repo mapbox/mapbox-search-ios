e43eda3b8fc6732a55c45b7abb537a8fcf4432f3
mapbox-ci <no-reply@mapbox.com>
2026-09-15 02:28:00 +0000

    [skip ci][no ci] Bump snapshot versions `SNAPSHOT-09-15--01-40.git-aa2d2d1`
    GitOrigin-RevId: 8a137374b0d538c90dd3a13f70c13466be7325ac

--- Sources/MapboxSearch/PublicAPI/MapboxSearchVersion.swift
+++ Sources/MapboxSearch/PublicAPI/MapboxSearchVersion.swift
@@ -1,2 +1,2 @@
 /// Mapbox Search SDK version variable
-public let mapboxSearchSDKVersion = "2.32.0-SNAPSHOT-09-14--15-28.git-15c49c5"
+public let mapboxSearchSDKVersion = "2.32.0-SNAPSHOT-09-15--01-40.git-aa2d2d1"
