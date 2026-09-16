f701a21b1af6f4040cc492e433b789bbc2557bdd
mapbox-ci <no-reply@mapbox.com>
2026-09-16 14:03:09 +0000

    [skip ci][no ci] Bump snapshot versions `SNAPSHOT-09-16--13-01.git-6d2e0d5`
    GitOrigin-RevId: 5023c6ef4a9a3c91f713b96300d6e9a104a37106

--- LICENSE.md
+++ LICENSE.md
@@ -1,6 +1,6 @@
 ## License
 
-Mapbox Search for iOS version 2.32.0-SNAPSHOT-09-16--01-38.git-c07ad61
+Mapbox Search for iOS version 2.32.0-SNAPSHOT-09-16--13-01.git-6d2e0d5
 Mapbox Search iOS SDK
 
 Copyright © 2021 - 2026 Mapbox, Inc. All rights reserved.
--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@
 import PackageDescription
 import Foundation
 
-let coreSearchVersion: Version = "2.32.0-SNAPSHOT-09-16--01-38.git-c07ad61"
-let mapboxCommonSDKVersion: Version = "24.32.0-SNAPSHOT-09-16--01-38.git-c07ad61"
+let coreSearchVersion: Version = "2.32.0-SNAPSHOT-09-16--13-01.git-6d2e0d5"
+let mapboxCommonSDKVersion: Version = "24.32.0-SNAPSHOT-09-16--13-01.git-6d2e0d5"
 
 let package = Package(
     name: "MapboxSearch",
